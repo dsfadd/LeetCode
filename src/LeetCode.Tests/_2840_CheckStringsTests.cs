0257cf1faacfd81858d66b2b95617f68f576845f
Егор Коликов <Egor882002@gmail.com>
2026-03-30 16:38:21 +0300

    add solution and tests for LeetCode 2840

---
+++ src/LeetCode.Tests/_2840_CheckStringsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Tests
+{
+    public class _2840_CheckStringsTests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var solver = new _2840_CheckStrings();
+            string s1 = "abe";
+            string s2 = "bea";
+
+            bool expected = false;
+
+            bool result = solver.Solve(s1,s2);
+
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void Example2()
+        {
+            var solver = new _2840_CheckStrings();
+            string s1 = "abcdba";
+            string s2 = "cabdab";
+
+            bool expected = true;
+
+            bool result = solver.Solve(s1,s2);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ src/LeetCode/_2840_CheckStrings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _2840_CheckStrings
+    {
+        public bool Solve(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                return false;
+
+            var count = new int[2, 26];
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                int parity = i & 1;
+
+                count[parity, s1[i] - 'a']++;
+                count[parity, s2[i] - 'a']--;
+            }
+
+            for (int p = 0; p < 2; p++)
+            {
+                for (int c = 0; c < 26; c++)
+                {
+                    if (count[p, c] != 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
