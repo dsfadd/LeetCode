a39c02f9ced6b88ed76e4eb747162f056bb757c2
Егор Коликов <Egor882002@gmail.com>
2026-03-30 16:16:50 +0300

    Создание репозитория

---
+++ src/LeetCode.Tests/_840_MagicSquaresTests.cs
@@ -0,0 +1,17 @@
+namespace LeetCode.Tests
+{
+    public class _840_MagicSquaresTests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var solver = new _840_MagicSquares();
+            int[][] grid = [[4, 3, 8, 4], [9, 5, 1, 9], [2, 7, 6, 2]];
+
+            int expected = 1;
+            int result=solver.Solve(grid);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ src/LeetCode/_840_MagicSquares.cs
@@ -0,0 +1,65 @@
+﻿namespace LeetCode
+{
+    public class _840_MagicSquares
+    {
+        public const string url = "https://leetcode.com/problems/magic-squares-in-grid/description/";
+
+        public int Solve(int[][] grid)
+        {
+            int rows = grid.Length;
+            int cols = grid[0].Length;
+            int count = 0;
+
+            for (int i = 0; i < rows - 2; i++)
+            {
+                for (int j = 0; j < cols - 2; j++)
+                {
+                    if (IsMagic(grid, i, j))
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        private bool IsMagic(int[][] g, int r, int c)
+        {
+            if (g[r + 1][c + 1] != 5)
+                return false;
+
+            int mask = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int val = g[r + i][c + j];
+
+                    if (val < 1 || val > 9)
+                        return false;
+
+                    int bit = 1 << val;
+
+                    if ((mask & bit) != 0)
+                        return false;
+
+                    mask |= bit;
+                }
+            }
+
+            int sum =
+                g[r][c] + g[r][c + 1] + g[r][c + 2];
+
+            return
+                sum == g[r + 1][c] + g[r + 1][c + 1] + g[r + 1][c + 2] &&
+                sum == g[r + 2][c] + g[r + 2][c + 1] + g[r + 2][c + 2] &&
+
+                sum == g[r][c] + g[r + 1][c] + g[r + 2][c] &&
+                sum == g[r][c + 1] + g[r + 1][c + 1] + g[r + 2][c + 1] &&
+                sum == g[r][c + 2] + g[r + 1][c + 2] + g[r + 2][c + 2] &&
+
+                sum == g[r][c] + g[r + 1][c + 1] + g[r + 2][c + 2] &&
+                sum == g[r][c + 2] + g[r + 1][c + 1] + g[r + 2][c];
+        }
+    }
+}
