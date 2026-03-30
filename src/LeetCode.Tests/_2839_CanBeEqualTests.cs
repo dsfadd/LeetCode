f9e5a236ceca2ebf76a045333b971882005cdd5b
Егор Коликов <Egor882002@gmail.com>
2026-03-30 16:38:11 +0300

    add solution and tests for LeetCode 2839

---
+++ src/LeetCode.Tests/_2839_CanBeEqualTests.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Tests
+{
+    public class _2839_CanBeEqualTests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var solver = new _2839_CanBeEqual();
+            string s1 = "abcd";
+            string s2 = "cdab";
+
+            bool expected = true;
+            bool result = solver.Solve(s1,s2);
+
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void Example2()
+        {
+            var solver = new _2839_CanBeEqual();
+            string s1 = "abcd";
+            string s2 = "dacb";
+
+            bool expected = false;
+            bool result = solver.Solve(s1,s2);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
