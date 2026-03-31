eba2a5c5053a12a42ad1cc27e8266fb9d0dbfffc
dsfadd <egork3017@gmail.com>
2026-03-31 08:07:59 +0300

    add solution and tests for LeetCode 2293

---
+++ src/LeetCode.Tests/_2293_MinMaxGameTests.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Tests
+{
+    public class _2293_MinMaxGameTests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var solver = new _2293_MinMaxGame();
+            int[] nums = [1, 3, 5, 2, 4, 8, 2, 2];
+
+            int expected = 1;
+            int result = solver.Solve(nums);
+
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void Example2()
+        {
+            var solver = new _2293_MinMaxGame();
+            int[] nums = [3];
+
+            int expected = 3;
+            int result = solver.Solve(nums);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ src/LeetCode/_2293_MinMaxGame.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode
+{
+    public class _2293_MinMaxGame
+    {
+        public const string url = "https://leetcode.com/problems/min-max-game/description/";
+
+        public int Solve(int[] nums)
+        {
+            int n = nums.Length;
+            if (n == 1) return nums[0];
+
+            int[] newNums = new int[n / 2];
+            int numsIndex = 0;
+            for (int i = 0; i < n / 2; i++)
+            {
+                if ((i & 1) == 0)
+                {
+                    newNums[i] = Min(nums[numsIndex], nums[numsIndex + 1]);
+                    numsIndex += 2;
+                }
+                else
+                {
+                    newNums[i] = Max(nums[numsIndex], nums[numsIndex + 1]);
+                    numsIndex += 2;
+                }
+            }
+
+            return Solve(newNums);
+        }
+        private int Min(int a, int b)
+        {
+            if (a < b) return a;
+            else return b;
+        }
+        private int Max(int a, int b)
+        {
+            if (a > b) return a;
+            else return b;
+        }
+    }
+}
