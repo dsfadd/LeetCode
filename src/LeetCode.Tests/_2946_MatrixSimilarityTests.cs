04a9da4c855f0eb73f5ba5587288e1117e2a7308
dsfadd <egork3017@gmail.com>
2026-03-31 07:44:29 +0300

    add solution and tests for LeetCode 2946

---
+++ src/LeetCode.Tests/_2946_MatrixSimilarityTests.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Tests
+{
+    public class _2946_MatrixSimilarityTests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var solver = new _2946_MatrixSimilarity();
+            int[][] mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+            int k = 4;
+
+            bool expected = false;
+            bool result = solver.Solve(mat, k);
+
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void Example2()
+        {
+            var solver = new _2946_MatrixSimilarity();
+            int[][] mat = [[1, 2, 1, 2], [5, 5, 5, 5], [6, 3, 6, 3]];
+            int k = 2;
+
+            bool expected = true;
+            bool result = solver.Solve(mat, k);
+
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void Example3()
+        {
+            var solver = new _2946_MatrixSimilarity();
+            int[][] mat = [[2, 2], [2, 2]];
+            int k = 3;
+
+            bool expected = true;
+            bool result = solver.Solve(mat, k);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ src/LeetCode/_2946_MatrixSimilarity.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode
+{
+    public class _2946_MatrixSimilarity
+    {
+        public const string url = "https://leetcode.com/problems/matrix-similarity-after-cyclic-shifts/description/";
+
+        public bool Solve(int[][] mat, int k)
+        {
+            int rows = mat.Length;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int cols = mat[i].Length;
+                int shift = k % cols;
+
+                for (int j = 0; j < cols; j++)
+                {
+                    int expectedIndex;
+
+                    if ((i & 1) == 0)
+                    {
+                        expectedIndex = (j + shift) % cols;
+                    }
+                    else
+                    {
+                        expectedIndex = (j - shift + cols) % cols;
+                    }
+
+                    if (mat[i][j] != mat[i][expectedIndex])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
