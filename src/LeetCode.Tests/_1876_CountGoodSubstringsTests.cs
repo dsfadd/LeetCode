f7c2bb9a18133dbbe00d59b4a4cf8c18deb12393
dsfadd <egork3017@gmail.com>
2026-03-31 08:00:01 +0300

    add solution and tests for LeetCode 1876

---
+++ src/LeetCode.Tests/_1876_CountGoodSubstringsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Tests
+{
+    public class _1876_CountGoodSubstringsTests
+    {
+        [Fact]
+        public void Example1()
+        {
+            var solver = new _1876_CountGoodSubstrings();
+            string s = "xyzzaz";
+
+            int expected = 1;
+            int result = solver.Solve(s);
+
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void Example2()
+        {
+            var solver = new _1876_CountGoodSubstrings();
+            string s = "aababcabc";
+
+            int expected = 4;
+            int result = solver.Solve(s);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ src/LeetCode/_1876_CountGoodSubstrings.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode
+{
+    public class _1876_CountGoodSubstrings
+    {
+        public const string url = "https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/description/";
+
+        public int Solve(string s)
+        {
+            int count = 0;
+            for (int i = 0; i <= s.Length - 3; i++)
+            {
+                if ((s[i] != s[i + 1]) && (s[i] != s[i + 2]) && (s[i + 1] != s[i + 2]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
